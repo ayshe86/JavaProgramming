2f835df24bb68b39366f9a7b0e177397750fce1d
Ayshe Mollamustafa <125811146+ayshe86@users.noreply.github.com>
2023-03-03 22:11:54 +0300

    day05_Concatenation

---
+++ src/day05_Concatenation/StudentInfo.java
@@ -0,0 +1,33 @@
+package day05_Concatenation;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String studentName = "Ayshe";
+        int age = 36;
+        char gender = 'F';
+        String schoolName = "Cydeo";
+        String phoneNumber = "(###)-###-####";
+        // long number = 1234564565;
+        boolean isFullTime = true;
+        double GPA = 2.5;
+
+        System.out.println("studentName = " + studentName);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("schoolName = " + schoolName);
+        System.out.println("phoneNumber = " + phoneNumber);
+        System.out.println("isFullTime = " + isFullTime);
+        System.out.println("GPA = " + GPA);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
