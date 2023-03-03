dd4fb71c46057e56b053725f2ab4a870688179c1
Ayshe Mollamustafa <125811146+ayshe86@users.noreply.github.com>
2023-03-03 20:44:30 +0300

    day04_Variables

--- src/day04_Variables/Currencies.java
+++ src/day04_Variables/Currencies.java
@@ -4,11 +4,18 @@ public class Currencies {
     public static void main(String[] args) {
          int dollar = 1000;
          double lira = dollar * 18.90;
-         double euro = dollar * 20.08;
+         double euro = dollar * 0.86;
+         double jpy = dollar * 114.14;
+         double pound = dollar * 0.73;
+         double CAD = dollar * 1.24;
+
          //jpy, pound, peso, canadianDollar, riyal...
         System.out.println("dollar = " + dollar);
         System.out.println("lira = " + lira);
         System.out.println("euro = " + euro);
+        System.out.println("CAD = " + CAD);
+        System.out.println("pound = " + pound);
+        System.out.println("jpy = " + jpy);
 
     }
 
