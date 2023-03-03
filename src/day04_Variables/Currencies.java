7ed8245f992ab3fbfb6c95218fc2267f47fba15e
Ayshe Mollamustafa <125811146+ayshe86@users.noreply.github.com>
2023-03-03 17:08:55 +0300

    commit Trying

---
+++ src/day04_Variables/Currencies.java
@@ -0,0 +1,17 @@
+package day04_Variables;
+
+public class Currencies {
+    public static void main(String[] args) {
+         int dollar = 1000;
+         double lira = dollar * 18.90;
+         double euro = dollar * 20.08;
+         //jpy, pound, peso, canadianDollar, riyal...
+        System.out.println("dollar = " + dollar);
+        System.out.println("lira = " + lira);
+        System.out.println("euro = " + euro);
+
+    }
+
+
+
+}
