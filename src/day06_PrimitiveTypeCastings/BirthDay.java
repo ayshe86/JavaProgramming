a79dba498db6a06af0319251d99b007afefee913
Ayshe Mollamustafa <125811146+ayshe86@users.noreply.github.com>
2023-03-04 21:10:54 +0300

    day06_Concatenation

---
+++ src/day06_PrimitiveTypeCastings/BirthDay.java
@@ -0,0 +1,19 @@
+package day06_PrimitiveTypeCastings;
+
+public class BirthDay {
+    public static void main(String[] args) {
+
+    }
+
+}
+/*
+
+			 	if  name = "John"
+			 		birthDay = 25
+			 	    birthMonth = "April"
+			 	    birthYear = 1995;
+
+			 	output:
+			 		John was born on April/25/1995.
+
+ */
